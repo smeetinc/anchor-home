5bc59c42c5e3f44a324d0821857c7d824d6b8d49
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2024-05-26 00:11:52 +0100

    working on hero and added aos animation

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useCallback } from "react";
 import "./Carousel.css"; // Make sure to create and import the CSS file
+import { Link } from "react-scroll";
 
 const Carousel = () => {
   const carouselRef = useRef(null);
@@ -74,80 +75,82 @@ const Carousel = () => {
           <div className="item bg-black bg-opacity-90">
             <img src="image/bedroom2.webp" alt="Slide 1" className="" />
             <div className="content">
-              <div className="author">LUNDEV</div>
-              <div className="title">DESIGN SLIDER</div>
-              <div className="topic">ANIMAL</div>
+              <div className="author">WELCOME TO</div>
+              <div className="title">ANCHOR'S</div>
+              <div className="topic uppercase text-primary-200">
+                Branston Lodge
+              </div>
               <div className="des">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
-                sequi, rem magnam nesciunt minima placeat, itaque eum neque
+                sequi, rem magnam nesciunt minima placeat. itaque eum neque
                 officiis unde, eaque optio ratione aliquid assumenda facere ab
-                et quasi ducimus aut doloribus non numquam. Explicabo,
-                laboriosam nisi reprehenderit tempora at laborum natus unde. Ut,
-                exercitationem eum aperiam illo illum laudantium?
               </div>
               <div className="buttons">
-                <button>SEE MORE</button>
-                <button>SUBSCRIBE</button>
+                <Link to="bedroom" smooth duration={500}>
+                  <button className="bg-primary-100 text-white">
+                    View Details
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
           <div className="item">
             <img src="image/lounge2.webp" alt="Slide 2" />
             <div className="content">
-              <div className="author">LUNDEV</div>
-              <div className="title">DESIGN SLIDER</div>
-              <div className="topic">ANIMAL</div>
+              <div className="author">WELCOME TO</div>
+              <div className="title">ANCHOR'S</div>
+              <div className="topic uppercase text-primary-200">
+                Branston Lodge
+              </div>
               <div className="des">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
                 sequi, rem magnam nesciunt minima placeat, itaque eum neque
                 officiis unde, eaque optio ratione aliquid assumenda facere ab
-                et quasi ducimus aut doloribus non numquam. Explicabo,
-                laboriosam nisi reprehenderit tempora at laborum natus unde. Ut,
-                exercitationem eum aperiam illo illum laudantium?
               </div>
               <div className="buttons">
-                <button>SEE MORE</button>
-                <button>SUBSCRIBE</button>
+                <Link to="lounge" smooth duration={500}>
+                  <button>View Details</button>
+                </Link>
               </div>
             </div>
           </div>
           <div className="item">
             <img src="image/mainHouse.webp" alt="Slide 3" />
-            <div className="content shadow-lg bg-black bg-opacity-75 px-4 py-5">
-              <div className="author">LUNDEV</div>
-              <div className="title">DESIGN SLIDER</div>
-              <div className="topic">ANIMAL</div>
+            <div className="content shadow-lg bg-black bg-opacity-75 px-8 py-8 w-full">
+              <div className="author">WELCOME TO</div>
+              <div className="title">ANCHOR'S</div>
+              <div className="topic uppercase text-primary-200">
+                Branston Lodge
+              </div>
               <div className="des">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
                 sequi, rem magnam nesciunt minima placeat, itaque eum neque
                 officiis unde, eaque optio ratione aliquid assumenda facere ab
-                et quasi ducimus aut doloribus non numquam. Explicabo,
-                laboriosam nisi reprehenderit tempora at laborum natus unde. Ut,
-                exercitationem eum aperiam illo illum laudantium?
               </div>
               <div className="buttons">
-                <button>SEE MORE</button>
-                <button>SUBSCRIBE</button>
+                <Link to="mainHouse" smooth duration={500}>
+                  <button>View Details</button>
+                </Link>
               </div>
             </div>
           </div>
           <div className="item">
             <img src="image/sittingRoom.webp" alt="Slide 4" />
             <div className="content">
-              <div className="author">LUNDEV</div>
-              <div className="title">DESIGN SLIDER</div>
-              <div className="topic">ANIMAL</div>
+              <div className="author">WELCOME TO</div>
+              <div className="title">ANCHOR'S</div>
+              <div className="topic uppercase text-primary-200">
+                Branston Lodge
+              </div>
               <div className="des">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
                 sequi, rem magnam nesciunt minima placeat, itaque eum neque
                 officiis unde, eaque optio ratione aliquid assumenda facere ab
-                et quasi ducimus aut doloribus non numquam. Explicabo,
-                laboriosam nisi reprehenderit tempora at laborum natus unde. Ut,
-                exercitationem eum aperiam illo illum laudantium?
               </div>
               <div className="buttons">
-                <button>SEE MORE</button>
-                <button>SUBSCRIBE</button>
+                <Link to="sittingRoom" smooth duration={500}>
+                  <button>View Details</button>
+                </Link>
               </div>
             </div>
           </div>
@@ -157,29 +160,35 @@ const Carousel = () => {
           <div className="item">
             <img src="image/bedroom2.webp" alt="Thumbnail 1" />
             <div className="content">
-              <div className="title">Name Slider</div>
-              <div className="description">Description</div>
+              <div className="title text-shadow-lg font-bold text-lg">
+                Bedroom
+              </div>
+              <div className="description"></div>
             </div>
           </div>
           <div className="item">
             <img src="image/lounge2.webp" alt="Thumbnail 2" />
             <div className="content">
-              <div className="title">Name Slider</div>
-              <div className="description">Description</div>
+              <div className="title text-shadow-lg font-bold text-lg">
+                {" "}
+                Our Lounge
+              </div>
             </div>
           </div>
           <div className="item">
             <img src="image/mainHouse.webp" alt="Thumbnail 3" />
             <div className="content">
-              <div className="title">Name Slider</div>
-              <div className="description">Description</div>
+              <div className="title text-shadow-lg font-bold text-lg">
+                Main House
+              </div>
             </div>
           </div>
           <div className="item">
             <img src="image/sittingRoom.webp" alt="Thumbnail 4" />
             <div className="content">
-              <div className="title">Name Slider</div>
-              <div className="description">Description</div>
+              <div className="title text-shadow-lg font-bold text-lg">
+                Sitting Room
+              </div>
             </div>
           </div>
         </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "../aos/dist/aos.css";
 
 function Section() {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
   return (
     <>
       <div name="section"></div>
@@ -9,7 +15,8 @@ function Section() {
         <h2 className="my-4 font-bold text-3xl">
           Sections of the Homes Available
         </h2>
-        <div className="flex flex-col lg:flex-row w-fit h-fit  items-center my-8 bg-primary-200 text-white">
+        <div name="sittingRoom"></div>
+        <div className="flex flex-col lg:flex-row w-fit h-fit  items-center my-8 ">
           <div className="w-fit h-fit rounded-s-lg">
             <img
               src="assets/img/sittingRoom.webp"
@@ -28,7 +35,8 @@ function Section() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col-reverse lg:flex-row w-fit h-fit  items-center my-8 bg-primary-200 text-white">
+        <div name="bedroom"></div>
+        <div className="flex flex-col-reverse lg:flex-row w-fit h-fit  items-center my-8 ">
           <div className="w-full h-full  py-auto px-12 ">
             <h3 className="text-center font-bold text-2xl my-4">Bed Room</h3>
             <p className="my-4">
@@ -40,22 +48,23 @@ function Section() {
 
           <div className="w-fit rounded-s-lg justify-self-end">
             <img
-              src="assets/img/bedroom.webp"
+              src="assets/img/bedroom2.webp"
               alt="home one"
               className="duration-1000 hover:scale-105 transition hover:duration-200 cursor-pointer w-full h-full object-contain"
             />
           </div>
         </div>
-        <div className="flex flex-col lg:flex-row w-fit h-fit  items-center my-8 bg-primary-200 text-white">
+        <div name="mainHouse"></div>
+        <div className="flex flex-col lg:flex-row w-fit h-fit  items-center my-8 ">
           <div className="w-fit rounded-s-lg">
             <img
-              src="assets/img/playArea.webp"
+              src="assets/img/mainHouse.webp"
               alt="home one"
               className="duration-1000 hover:scale-105 transition hover:duration-200 cursor-pointer w-full h-full object-contain"
             />
           </div>
           <div className="w-full h-full  py-auto px-12 ">
-            <h3 className="text-center font-bold text-2xl my-4">Play Area</h3>
+            <h3 className="text-center font-bold text-2xl my-4">Main House</h3>
             <p className="my-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
               id quam ea laboriosam sint illum voluptatibus culpa odio
@@ -63,21 +72,43 @@ function Section() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col-reverse lg:flex-row w-fit h-fit  items-center my-8 bg-primary-200 text-white">
+        <div name="lounge"></div>
+        <div className="flex flex-col-reverse lg:flex-row w-fit h-fit  items-center my-8  ">
           <div className="w-full h-full  py-auto px-12 ">
-            <h3 className="text-center font-bold text-2xl my-4">Lounge</h3>
-            <p className="my-4">
+            <h3
+              className="text-center font-bold text-2xl my-4"
+              data-aos="fade-zoom-in"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="600"
+            >
+              Lounge
+            </h3>
+            <p
+              className="my-4"
+              data-aos="fade-zoom-in"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="600"
+              data-aos-delay="300"
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
               id quam ea laboriosam sint illum voluptatibus culpa odio
               voluptatem repudiandae.
             </p>
           </div>
 
-          <div className="w-fit rounded-s-lg justify-self-end">
+          <div
+            className="w-fit rounded-s-lg justify-self-end"
+            data-aos="fade-left"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="800"
+          >
             <img
-              src="assets/img/lounge1.webp"
+              src="assets/img/lounge2.webp"
               alt="home one"
-              className="duration-1000 hover:scale-105 transition hover:duration-200 cursor-pointer w-full h-full object-contain"
+              className="duration-1000 hover:scale-105 transition hover:duration-500 cursor-pointer w-full h-full object-contain shadow-lg rounded-md"
             />
           </div>
         </div>
